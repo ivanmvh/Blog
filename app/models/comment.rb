9649a8982cf902c2c3a0426785f7e61be599e19c
Ivan Martinez von Halle <ivanmvh@gmail.com>
2023-04-07 13:18:16 -0500

    Fix typo in comments.rb

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,12 +1,10 @@
 class Comment < ApplicationRecord
-  belongs_to :author, class_name: 'User'
   belongs_to :post, class_name: 'Post'
+  belongs_to :author, class_name: 'User'
 
-  after_save :update_comment_counter
+  after_save :update_comments_counter
 
-  private
-
-  def update_comment_counter
+  def update_comments_counter
     post.update(comments_counter: post.comments.count)
   end
 end
