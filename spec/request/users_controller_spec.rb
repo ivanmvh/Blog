f9f3ab9aa34dd0c346ebfc7aeed74495f36e836a
Ivan Martinez von Halle <ivanmvh@gmail.com>
2023-04-12 01:03:25 -0500

    Implement rspec type: controller in add to type: request with educational purpose

--- spec/request/users_controller_spec.rb
+++ spec/request/users_controller_spec.rb
@@ -1,32 +1,30 @@
 require 'rails_helper'
 
-RSpec.describe UsersController, type: :controller do
-  render_views
-
-  describe 'GET /show' do
+RSpec.describe 'Users', type: :request do
+  describe 'GET /users' do
     context 'users should render correctly' do
       it 'renders a successful response' do
-        get :index
+        get users_path
         expect(response).to be_successful
       end
 
       it 'renders the correct template' do
-        get :index
+        get users_path
         expect(response).to render_template(:index)
       end
 
       it 'renders the correct template' do
-        get :show, params: { id: 4 }
+        get user_path(id: 4)
         expect(response).to render_template(:show)
       end
 
       it 'renders the view with the correct placeholder text' do
-        get :index
+        get users_path
         expect(response.body).to include('List of users')
       end
 
       it 'renders the view with the correct placeholder text' do
-        get :show, params: { id: 4 }
+        get user_path(id: 4)
         expect(response.body).to include('Information of a User')
       end
     end
