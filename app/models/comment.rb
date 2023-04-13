2886030380e9ccc055da86e30516722013c6c5f0
Ivan Martinez von Halle <ivanmvh@gmail.com>
2023-04-13 01:50:00 -0500

    Update action in models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,12 @@
 class Comment < ApplicationRecord
-  belongs_to :post, class_name: 'Post'
   belongs_to :author, class_name: 'User'
+  belongs_to :post
 
-  after_save :update_comments_counter
+  after_save :update_comment_counter
 
-  def update_comments_counter
-    post.update(comments_counter: post.comments.count)
+  private
+
+  def update_comment_counter
+    post.increment!(:comments_counter)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
-  has_many :posts, foreign_key: 'author_id', dependent: :destroy
-  has_many :comments, foreign_key: 'author_id', dependent: :destroy
-  has_many :likes, foreign_key: 'author_id', dependent: :destroy
+  has_many :posts, foreign_key: 'author_id'
+  has_many :comments, foreign_key: 'author_id'
+  has_many :likes, foreign_key: 'author_id'
 
-  validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
-  validates :name, presence: true
-
-  def most_recent_posts
-    posts.order(created_at: :desc).limit(3)
+  def self.most_recent_posts(name)
+    p Post.where(author_id: User.find_by(name:).id).order(created_at: :desc).limit(3)
   end
+
+  validates :name, presence: true
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
