41f4c08d62cb76dd4f558a3d88d19428f3f73e9b
Ivan Martinez von Halle <ivanmvh@gmail.com>
2023-04-14 22:07:09 -0500

    Add button Add a Post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
   def index
-    p @users = User.all
+    @users = User.all
     # render :index
   end
 
   def show
     @user = User.find(params[:id])
-    render :show
+    # render :show
   end
 end
