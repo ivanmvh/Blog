1a0b8d71626fb0bc684adf1a99ca6a18de979d4c
Ivan Martinez von Halle <ivanmvh@gmail.com>
2023-04-15 01:06:07 -0500

    Create Post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
-    # render :index
+    render :index
   end
 
   def show
     @user = User.find(params[:id])
-    # render :show
+    render :show
   end
 end
