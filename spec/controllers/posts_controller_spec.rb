3f61a2f744d2b2afe8528a910d1f622c7e1740dd
Ivan Martinez von Halle <ivanmvh@gmail.com>
2023-04-18 16:17:56 -0500

    Changes to spec/controllers

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,36 +1,53 @@
 require 'rails_helper'
 
-RSpec.describe PostsController, type: :controller do
-  # render_views
-
+RSpec.describe PostsController, type: :request do
   describe 'GET /show' do
     context 'posts should render correctly' do
+      user = User.create(name: 'Poly Connor', photo: '5-photo.jpg',
+                         bio: 'Teacher from Poland.', posts_counter: 0)
+      post = Post.create(author: user, title: 'Hello', text: 'This is my first post', comments_counter: 0,
+                         likes_counter: 0)
       it '1-renders a successful response' do
-        get :index, params: { user_id: 1 }
+        get "/users/#{user.id}/posts"
         expect(response).to be_successful
       end
 
       it '2-renders the index template' do
-        user = User.create(name: 'Lilly', photo: '1-photo', bio: 'Teacher from Poland.',
-                           posts_counter: 0)
-        get :index, params: { user_id: user.id }
-        expect(response).not_to render_template(:index)
+        user = User.create(name: 'Poly Connor', photo: '5-photo.jpg',
+                           bio: 'Teacher from Poland.', posts_counter: 0)
+        post = Post.create(author: user, title: 'Hello', text: 'This is my first post', comments_counter: 0,
+                           likes_counter: 0)
+        get "/users/#{user.id}/posts"
+        expect(response).to render_template(:index)
       end
 
       it '3-renders the index template' do
-        get :index, params: { user_id: 1 }  
-        p response.body
-        expect(response.body).to include('Posts List')
+        user = User.create(name: 'Poly Connor', photo: '5-photo.jpg',
+                           bio: 'Teacher from Poland.', posts_counter: 0)
+        post = Post.create(author: user, title: 'Hello', text: 'This is my first post', comments_counter: 0,
+                           likes_counter: 0)
+        get "/users/#{user.id}/posts"
+        expect(response.body).to include('Poly Connor')
       end
+    end
 
+    context 'posts should contain the correct information' do
       it '4-renders the show template' do
-        get :show, params: { user_id: 1, id: 1 }
+        user = User.create(name: 'Poly Connor', photo: '5-photo.jpg',
+                           bio: 'Teacher from Poland.', posts_counter: 0)
+        post = Post.create(author: user, title: 'Hello', text: 'This is my first post', comments_counter: 0,
+                           likes_counter: 0)
+        get "/users/#{user.id}/posts/#{post.id}"
         expect(response).to render_template(:show)
       end
 
       it '5-renders the show template' do
-        get :show, params: { user_id: 1, id: 1 }
-        expect(response.body).to include('Post Info')
+        user = User.create(name: 'Poly Connor', photo: '5-photo.jpg',
+                           bio: 'Teacher from Poland.', posts_counter: 0)
+        post = Post.create(author: user, title: 'Hello', text: 'This is my first post', comments_counter: 0,
+                           likes_counter: 0)
+        get "/users/#{user.id}/posts/#{post.id}"
+        expect(response.body).to include('This is my first post')
       end
     end
   end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -14,24 +14,24 @@ RSpec.describe 'Users', type: :request do
       end
 
       it '3-renders the correct template' do
-        user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+        user = User.create(name: 'Uber Carl', photo: '4-photo.jpg',
                            bio: 'Teacher from Poland.', posts_counter: 0)
         get user_path(id: user.id)
         expect(response).to render_template(:show)
       end
 
       it '4-renders the view with the correct placeholder text' do
-        User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+        User.create(name: 'Uber Carl', photo: '4-photo.jpg',
                     bio: 'Teacher from Poland.', posts_counter: 0)
         get users_path
-        expect(response.body).to include('Lilly')
+        expect(response.body).to include('Uber Carl')
       end
 
       it '5-renders the view with the correct placeholder text' do
-        user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+        user = User.create(name: 'Uber Carl', photo: '4-photo.jpg',
                            bio: 'Teacher from Poland.', posts_counter: 0)
         get user_path(id: user.id)
-        expect(response.body).to include('Lilly')
+        expect(response.body).to include('Uber Carl')
       end
     end
   end
