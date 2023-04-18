9f66dc1933fb661682e897ed5ee692ea4cb4846e
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2023-04-18 09:38:25 -0600

    Add test users controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,33 +1,37 @@
 require 'rails_helper'
 
-RSpec.describe UsersController, type: :controller do
-  render_views
-
-  describe 'GET /show' do
+RSpec.describe 'Users', type: :request do
+  describe 'GET /users' do
     context 'users should render correctly' do
       it '1-renders a successful response' do
-        get :index
+        get users_path
         expect(response).to be_successful
       end
 
       it '2-renders the correct template' do
-        get :index
+        get users_path
         expect(response).to render_template(:index)
       end
 
       it '3-renders the correct template' do
-        get :show, params: { id: 1 }
+        user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                           bio: 'Teacher from Poland.', posts_counter: 0)
+        get user_path(id: user.id)
         expect(response).to render_template(:show)
       end
 
       it '4-renders the view with the correct placeholder text' do
-        get :index
-        expect(response.body).to include('Users List')
+        User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                    bio: 'Teacher from Poland.', posts_counter: 0)
+        get users_path
+        expect(response.body).to include('Lilly')
       end
 
       it '5-renders the view with the correct placeholder text' do
-        get :show, params: { id: 1 }
-        expect(response.body).to include('Users Info')
+        user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                           bio: 'Teacher from Poland.', posts_counter: 0)
+        get user_path(id: user.id)
+        expect(response.body).to include('Lilly')
       end
     end
   end
