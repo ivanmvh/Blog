8de4ed5649b28d144b3eb149afe1d95e9dd29e16
Ivan Martinez von Halle <ivanmvh@gmail.com>
2023-04-19 19:52:30 -0500

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,22 @@
 class ApplicationController < ActionController::Base
-  def self.current_user
-    User.first
+  protect_from_forgery with: :exception
+
+  before_action :update_allowed_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
+  end
+
+  #  de-sam
+  #  before_action :authenticate_user!
+  #  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: %i[name photo bio])
   end
 end
